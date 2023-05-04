72e25afb23385cce5649898d9c2b51c6dee79cf4
jeanccdev <jeanccdev@gmail.com>
2023-05-03 21:34:32 -0300

    Teste

--- db.js
+++ db.js
@@ -1,14 +1,6 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DBNAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect((error) => {
   if (error) throw error;
